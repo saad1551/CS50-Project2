e0cb53920a9e84ff2e2ac4218ebace1f68264d55
saad1551 <saad.ashraf.9094@gmail.com>
2023-08-22 11:18:47 +0500

    models almost done, though there might be an issue with the choice validation in the category model

--- auctions/models.py
+++ auctions/models.py
@@ -6,8 +6,8 @@ class User(AbstractUser):
     pass
 
 class Category(models.Model):
-    category_choices = ["Fashion", "Toys", "Electronics", "Home"]
-    name = models.CharField(choices=category_choices, unique=True)
+    category_choices = [("fashion", "Fashion"), ("toys", "Toys"), ("electronics", "Electronics"), ("home", "Home")]
+    name = models.CharField(choices=category_choices, unique=True, max_length=50)
 
 class Listing(models.Model):
     title = models.CharField(max_length=80)
@@ -16,6 +16,9 @@ class Listing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     imageUrl = models.URLField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="listings", blank=True, null=True)
+
+    def __str__(self):
+        return f"title: {self.title}\nstarting_bid={self.starting_bid}\ncategory:{self.category}"
 
 class Bid(models.Model):
     listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
