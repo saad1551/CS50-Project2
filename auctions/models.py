63e7b8dca920bc0352815e47ad8c2a0a0c6a954c
saad1551 <saad.ashraf.9094@gmail.com>
2023-08-22 10:54:29 +0500

    created a category model and modified the existing ones

--- auctions/models.py
+++ auctions/models.py
@@ -5,17 +5,24 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Category(models.Model):
+    category_choices = ["Fashion", "Toys", "Electronics", "Home"]
+    name = models.CharField(choices=category_choices, unique=True)
+
 class Listing(models.Model):
     title = models.CharField(max_length=80)
-    description = models.TextField
-    starting_bid = models.FloatField
+    description = models.TextField()
+    starting_bid = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    imageUrl = models.URLField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="listings", blank=True, null=True)
 
 class Bid(models.Model):
     listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
-    bid_amount = models.FloatField
+    bid_amount = models.FloatField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
 
 class Comment(models.Model):
     listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
-    comment = models.TextField
+    comment = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
