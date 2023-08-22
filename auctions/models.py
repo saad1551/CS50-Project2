3dcf1fc7afb045f3561429e4559471954a01bfcd
saad1551 <saad.ashraf.9094@gmail.com>
2023-08-22 09:42:35 +0500

    create models for listing, bid, comment

--- auctions/models.py
+++ auctions/models.py
@@ -9,3 +9,13 @@ class Listing(models.Model):
     title = models.CharField(max_length=80)
     description = models.TextField
     starting_bid = models.FloatField
+
+class Bid(models.Model):
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    bid_amount = models.FloatField
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+
+class Comment(models.Model):
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    comment = models.TextField
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
