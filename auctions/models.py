0cc0399e29c14d383b174b3a6ac051f58a7d680a
saad1551 <saad.ashraf.9094@gmail.com>
2023-11-14 21:11:38 +0500

    created the Message model

--- auctions/models.py
+++ auctions/models.py
@@ -49,6 +49,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.user.username}\n{self.comment}"
+    
+
+class Message(models.Model):
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
+    message = models.TextField()
 
 
 
